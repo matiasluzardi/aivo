2f0bdd59db663a5e78bc96e90f0e3ee6e33ae819
Matias <matias.luzardi@globant.com>
2018-01-02 01:19:18 -0300

    facebook found

--- src/routes.php
+++ src/routes.php
@@ -5,11 +5,39 @@ use Slim\Http\Response;
 
 // Routes
 
+//1348339814
 $app->get('/profile/facebook/{id}', function (Request $request, Response $response, array $args) {
-    // Sample log message
+
+  $id = $request->getAttribute('id');
+
+  $fb = new \Facebook\Facebook([
+  'app_id' => '2084223635139334',
+  'app_secret' => 'cdcda49183a048a820154e0db4ed0343',
+  'default_graph_version' => 'v2.11'
+//  'default_access_token' => 'cdcda49183a048a820154e0db4ed0343', // optional
+]);
+    	
+$helper = $fb->getRedirectLoginHelper(); 
+try {
+
+  $accessToken = $helper->getAccessToken(); 
+  // Returns a `Facebook\FacebookResponse` object
+   $responseFacebook = $fb->get('/'.$id.'?fields=id,first_name,last_name', "EAAdnlyFnqwYBAK4GXBMyEwmGFFODiO8uTNyRDENCvlAypPlB29GVEavfcPgxMdSHRukzkgFPBzzMnZAeraziAkNTZBXEsJVWNrqmWx4XTL3scVCLP1XV2kKBG5Bs46JLd912dKPZA9ZBZCaziNoyK68DQxjJnZCw0PkWh3nK5uNnXGmG9fuksRycOaDBsHDsDgAIBuVXCQqQZDZD");
+
+} catch(\Facebook\Exceptions\FacebookResponseException $e) {
+  // When Graph returns an error
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(\Facebook\Exceptions\FacebookSDKException $e) {
+  // When validation fails or other local issues
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+	
     $this->logger->info("Slim-Skeleton '/' route");
-    $id = $request->getAttribute('id');
-    $responseData = ["id"=>$id,"firstName"=>"Juan","lastName"=>"Perez"];
+	$me = $responseFacebook->getGraphUser();
+   
+    $responseData = ["id"=>$me->getId(),"firstName"=>$me->getFirstName(),"lastName"=>$me->getLastName()];
     // Render index view
     return $this->response->withJson($responseData,200);
 });

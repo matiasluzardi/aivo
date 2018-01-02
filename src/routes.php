98fe34e4fc6e66d5415927054e4a5298fa6c7606
Matias <matias.luzardi@globant.com>
2018-01-02 00:19:54 -0300

    ws

--- src/routes.php
+++ src/routes.php
@@ -5,10 +5,11 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+$app->get('/profile/facebook/{id}', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
-
+    $id = $request->getAttribute('id');
+    $responseData = ["id"=>$id,"firstName"=>"Juan","lastName"=>"Perez"];
     // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    return $this->response->withJson($responseData,200);
 });
